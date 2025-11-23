4b76a822046b96576c72b83bba7adf239d136eef
Jaikar Tridandapani <tridandapani.jaikar21@gmail.com>
2025-11-22 20:24:53 -0500

    Refactor database connection to use environment variables

--- 3-Tier-NodeJS-MySql-Local/3-Tier-NodeJS-MySql-Local-main/server/server.js
+++ 3-Tier-NodeJS-MySql-Local/3-Tier-NodeJS-MySql-Local-main/server/server.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json());
 
 // Database connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'test_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
